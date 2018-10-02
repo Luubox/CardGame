ddf2af5a557737cc35b52f9432b958198dc15bde
J Nielsen <buhros@hotmail.com>
2018-10-02 10:25:41 +0200

    Initial commit

---
+++ CardGame/Deck.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CardGame
+{
+    class Deck
+    {
+        Card[] cards = new Card[76];
+
+        public Deck()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                cards[i] = new Card("Red", i);
+                cards[i + 9] = new Card("Red", i);
+
+                cards[i + 19] = new Card("Blue", i);
+                cards[i + 28] = new Card("Blue", i);
+
+                cards[i + 38] = new Card("Green", i);
+                cards[i + 47] = new Card("Green", i);
+
+                cards[i + 57] = new Card("Yellow", i);
+                cards[i + 66] = new Card("Yellow", i);
+            }
+
+            for (int i = 0; i < cards.Length; i++)
+            {
+                Console.WriteLine(cards[i]);
+            }
+        }
+    }
+}
